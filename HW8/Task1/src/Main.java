911d99181dec187aa4eba63b75f92bf79835dfd6
ximik28 <ximik28@tut.by>
2022-12-16 22:02:46 +0300

    Выполнен Task1

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,38 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt(320);
+        }
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 }
